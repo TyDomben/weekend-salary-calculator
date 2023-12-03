c158268b2f9eabad57bbd2fe0eb446fad476a2d3
Ty Domben <127000572+TyDomben@users.noreply.github.com>
2023-12-03 16:58:19 -0600

    just a generic save point

--- script.js
+++ script.js
@@ -1,53 +1,60 @@
 document.addEventListener('DOMContentLoaded', function() {
+    console.log("DOM fully loaded");
+
     // Handle form submission
     document.getElementById('employeeForm').addEventListener('submit', function(event) {
         event.preventDefault();
+        console.log("WELCOME!WELCOME!WELCOME!");
 
         // Capture form data
         const firstName = document.getElementById('firstName').value;
         const lastName = document.getElementById('lastName').value;
-        // ... capture other fields
+        // ... capture other fields as needed
 
-        // Validate data (example)
-        if (firstName === '' || lastName === '') {
-            alert('Please fill in all fields');
+        console.log(`Form Data: ${firstName}, ${lastName}, ...`);
+
+        // Validate data
+        // if either firstName or lastname are empty I will scream - (the console that is).
+        if (firstName === "" || lastName === "") {
+            console.log('SCREAMING INTESIFIES');
             return;
         }
 
-        // Add employee to the table
-        addEmployeeToTable(firstName, lastName /*, other fields */);
+        // Add employee data to the table 
+        // For now, just logging the action
+        console.log(`Adding employee: ${firstName} ${lastName}`);
 
         // Clear form fields
         this.reset();
+        console.log("Form fields cleared");
 
-        // Update total monthly cost
-        updateTotalMonthlyCost();
+        // Update total monthly cost 
+        // For now, just log the action
+        console.log("Updating total monthly cost");
     });
 
     // Function to add employee to the table
-    function addEmployeeToTable(firstName, lastName /*, other fields */) {
-        const table = document.getElementById('employeeTable');
-        const row = table.insertRow();
-        row.innerHTML = `
-            <td>${firstName}</td>
-            <td>${lastName}</td>
-            // ... other cells
-            <td><button class="deleteBtn" onclick="deleteEmployee(this)">Delete</button></td>
-        `;
+    function addEmployeeToTable(firstName, lastName /* ... other parameters */) {
+        // Implementation would go here
+        console.log(`Function to add ${firstName} ${lastName} to the table called`);
     }
 
     // Function to update total monthly cost
     function updateTotalMonthlyCost() {
-        // Logic to calculate and update total
+        // Implementation would go here
+        console.log("Function to update total monthly cost called");
     }
+    // TODO: Calculate the total monthly cost
+    // TODO: Update the displayed total
 
     // Function to delete an employee
-    window.deleteEmployee = function(button) {
-        const row = button.parentElement.parentElement;
-        document.getElementById('employeeTable').removeChild(row);
-        updateTotalMonthlyCost();
-    };
+    function deleteEmployee(button) {
+        // Implementation would go here
+        console.log("Function to delete an employee called");
+    }
 });
+
+
 
 
 //what do i want the code to do?
@@ -55,6 +62,7 @@ document.addEventListener('DOMContentLoaded', function() {
 //How?  
 //-> Console logs.
 //   buttons, 
+
 
 // <form id="employeeForm">
 // <label for="firstName">First Name:</label>
