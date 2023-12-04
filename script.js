1b3a056e3fd7c5446601f048553d7221cf57d4f0
Ty Domben <127000572+TyDomben@users.noreply.github.com>
2023-12-03 21:27:09 -0600

    complete

--- script.js
+++ script.js
@@ -1,87 +1,87 @@
-document.addEventListener('DOMContentLoaded', function() {
+// Function to update total monthly cost
+function updateTotalMonthlyCost() {
+    const totalMonthlyElement = document.getElementById('totalMonthly');
+    const salaryCells = document.querySelectorAll('#employeeTable td:nth-child(5)');
+    let totalAnnualSalary = 0;
+    salaryCells.forEach(function (cell) {
+        const salary = Number(cell.textContent) || 0;
+        totalAnnualSalary += salary;
+    });
+    const totalMonthlyCost = totalAnnualSalary / 12;
+    document.getElementById('totalMonthly').textContent = totalMonthlyCost.toFixed(2);
+    // Check if over budget
+    if (totalMonthlyCost > 20000) {
+        totalMonthlyElement.classList.add('over-budget');
+        // } else {
+        //     totalMonthlyElement.classList.remove('over-budget');
+        // 
+    }
+}
+
+
+// Function to add employee to the table
+function addEmployeeToTable(firstName, lastName, idNumber, jobTitle, annualSalary) {
+    const table = document.getElementById('employeeTable');
+    const row = table.insertRow();
+    row.innerHTML = `
+        <td>${firstName}</td>
+        <td>${lastName}</td>
+        <td>${idNumber}</td>
+        <td>${jobTitle}</td>
+        <td>${annualSalary}</td>
+        <td><button class="deleteBtn">Delete</button></td>
+    `;
+
+    // Add event listener to the new delete button
+    row.querySelector('.deleteBtn').addEventListener('click', function () {
+        deleteEmployee(this);
+    });
+}
+
+// Function to delete an employee
+function deleteEmployee(button) {
+    const row = button.parentElement.parentElement;
+    document.getElementById('employeeTable').removeChild(row);
+    updateTotalMonthlyCost();
+}
+
+// Event listener for DOMContentLoaded
+document.addEventListener('DOMContentLoaded', function () {
     console.log("DOM fully loaded");
 
     // Handle form submission
-    document.getElementById('employeeForm').addEventListener('submit', function(event) {
+    document.getElementById('employeeForm').addEventListener('submit', function (event) {
         event.preventDefault();
-        console.log("WELCOME!WELCOME!WELCOME!");
 
         // Capture form data
         const firstName = document.getElementById('firstName').value;
         const lastName = document.getElementById('lastName').value;
-        // ... capture other fields as needed
-
-        console.log(`Form Data: ${firstName}, ${lastName}, ...`);
+        const idNumber = document.getElementById('idNumber').value;
+        const jobTitle = document.getElementById('jobTitle').value;
+        const annualSalary = document.getElementById('annualSalary').value;
 
         // Validate data
-        // if either firstName or lastname are empty I will scream - (the console that is).
-        if (firstName === "" || lastName === "") {
-            console.log('SCREAMING INTESIFIES');
+        if (firstName === "" || lastName === "" || idNumber === "" || jobTitle === "" || annualSalary === "" || annualSalary < 0) {
+            console.log('Please fill in all fields correctly');
             return;
         }
 
-        // Add employee data to the table 
-        // For now, just logging the action
-        console.log(`Adding employee: ${firstName} ${lastName}`);
-
+        // Add employee data to the table and update total monthly cost
+        addEmployeeToTable(firstName, lastName, idNumber, jobTitle, annualSalary);
+        updateTotalMonthlyCost();
         // Clear form fields
         this.reset();
-        console.log("Form fields cleared");
 
-        // Update total monthly cost 
-        // For now, just log the action
-        console.log("Updating total monthly cost");
     });
 
-    // Function to add employee to the table
-    function addEmployeeToTable(firstName, lastName /* ... other parameters */) {
-        // Implementation would go here
-        console.log(`Function to add ${firstName} ${lastName} to the table called`);
-    }
+    // Add event listeners to initially existing delete buttons
+    const deleteButtons = document.querySelectorAll('.deleteBtn');
+    deleteButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            deleteEmployee(this);
+        });
+    });
 
-    // Function to update total monthly cost
-    function updateTotalMonthlyCost() {
-        // Implementation would go here
-        console.log("Function to update total monthly cost called");
-    }
-    // TODO: Calculate the total monthly cost
-    // TODO: Update the displayed total
-
-    // Function to delete an employee
-    function deleteEmployee(button) {
-        // Implementation would go here
-        console.log("Function to delete an employee called");
-    }
+    // Initial calculation for any pre-existing employees in the table
+    updateTotalMonthlyCost();
 });
-
-
-
-
-//what do i want the code to do?
-//
-//How?  
-//-> Console logs.
-//   buttons, 
-
-
-// <form id="employeeForm">
-// <label for="firstName">First Name:</label>
-// <input type="text" id="firstName" data-testid="firstNameInput">
-
-// <label for="lastName">Last Name:</label>
-// <input type="text" id="lastName" data-testid="lastNameInput">
-
-// <label for="idNumber">ID Number:</label>
-// <input type="text" id="idNumber" data-testid="idInput">
-
-// <label for="jobTitle">Job Title:</label>
-// <input type="text" id="jobTitle" data-testid="titleInput">
-
-// <label for="annualSalary">Annual Salary:</label>
-// <input type="number" id="annualSalary" data-testid="annualSalaryInput">
-
-// <button type="submit" data-testid="submitButton">Submit</button>
-// </form>
-/* <tbody id="employeeTable">
-<!-- Employee rows will be added here dynamically?? -->
-</tbody> */
